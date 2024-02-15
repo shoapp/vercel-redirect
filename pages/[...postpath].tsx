ff26680658b8462fafe5b92a4eb2e8f8b0f46152
shoapp <125400285+shoapp@users.noreply.github.com>
2024-02-15 17:38:41 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,8 +4,7 @@ import { GetServerSideProps } from "next";
 import { GraphQLClient, gql } from "graphql-request";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const endpoint = process.env.GRAPHQL_ENDPOINT as string;
-  const graphQLClient = new GraphQLClient(endpoint);
+  const endpoint = "https://xposed-magazine.com/graphql"
   const referringURL = ctx.req.headers?.referer || null;
   const pathArr = ctx.query.postpath as Array<string>;
   const path = pathArr.join("/");
